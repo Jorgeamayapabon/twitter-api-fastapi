ce0fb1980b99e8e8eba844599b3f6a907463705a
JORGE ENRIQUE AMAYA PABON <jeap1723@gmail.com>
2022-11-22 10:18:54 -0500

    home

--- main.py
+++ main.py
@@ -122,6 +122,8 @@ def login():
 )
 def show_all_users():
     """
+    Show All Users
+
     This path operation shows all users in the app
 
     Parameters:
@@ -183,7 +185,24 @@ def update_a_user():
 
 )
 def home():
-    return {"Twitter API": "Working!"}
+    """
+    Home
+
+    This path operation shows all tweets in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all tweets in the app, with the following keys
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Post a tweet
 @app.post(
